5b15720405578f6a3d26e48283823fbc4c81c30a
Peter Petrov <pp.petrov94@gmail.com>
2021-03-27 15:33:26 +0200

    Implementing Footer component

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 import Contacts from "./components/Contacts/Contacts";
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route path="/about" exact component={About} />
           <Route path="/contacts" exact component={Contacts} />
         </Switch>
+        <Footer />
       </BrowserRouter>
     </div>
   );
