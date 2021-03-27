df0a66c1ffcb322aeb9184be1135805775879421
Peter Petrov <pp.petrov94@gmail.com>
2021-03-27 15:59:12 +0200

    Implementing css for Categories component

--- client/src/components/Categories/Categories.js
+++ client/src/components/Categories/Categories.js
@@ -4,6 +4,13 @@ const Categories = () => {
   return (
     <div className="categories-container">
       <h1>Categories</h1>
+      <div className="category-index">
+        <p>First Category</p>
+        <p>Second Category</p>
+        <p>Third Category</p>
+        <p>Fourth Category</p>
+        <p>Fifth Category</p>
+      </div>
     </div>
   );
 };
