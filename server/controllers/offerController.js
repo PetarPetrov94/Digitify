f557fca3debe3260456979d18c057893a1c2b016
Peter Petrov <pp.petrov94@gmail.com>
2021-04-07 13:58:35 +0300

    Update offerController and offerService on BE
    Implementing logic for offer being saved in database

--- server/controllers/offerController.js
+++ server/controllers/offerController.js
@@ -1,8 +1,17 @@
 const router = require("express").Router();
 const offerService = require("../services/offerService");
 
-router.post("/", (req, res) => {
-  const data = req.body;
+router.post("/", async (req, res) => {
+  try {
+    let offerTocreate = await offerService.createOffer(req.body);
+    res.status(200).json({
+      message: "Offer created successfully!",
+      offerTocreate,
+    });
+  } catch (error) {
+    res.sendStatus(404);
+    console.log(error);
+  }
 });
 
 module.exports = router;
