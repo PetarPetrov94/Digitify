3e376714b55b5bc9e58e10b505741b2d977231ee
Peter Petrov <pp.petrov94@gmail.com>
2021-04-10 19:24:36 +0300

    Implementing TV render logic

--- client/src/components/TVs/TVs.js
+++ client/src/components/TVs/TVs.js
@@ -1,8 +1,46 @@
+import { useEffect, useState } from "react";
+import { Helmet } from "react-helmet";
+import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
+
+import * as categoryService from "../../services/categoryService";
 import "./TVs.css";
+
 const TVs = () => {
+  const [tvs, setTVs] = useState([]);
+  const [fetchingState, setFetchingState] = useState(true);
+
+  useEffect(() => {
+    setFetchingState(true);
+
+    categoryService.findTvOffers().then((data) => {
+      setTVs(data);
+      setFetchingState(false);
+    });
+  }, []);
+
+  if (fetchingState) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <div className="tv-container">
-      <h1>Laptops</h1>
+      <Helmet>
+        <title>Digitify | Phones</title>
+      </Helmet>
+      <h1>Phones</h1>
+      {tvs.map((tv, index) => {
+        return (
+          <Card key={index} className="tv-body">
+            <CardImg key={index} className="tv-image" src={tv.productImage} />
+            <CardBody>
+              <CardTitle key={index} tag="h5">
+                {tv.productName}
+              </CardTitle>
+              <CardText key={index}>{tv.productDescription}</CardText>
+            </CardBody>
+          </Card>
+        );
+      })}
     </div>
   );
 };
--- client/src/services/categoryService.js
+++ client/src/services/categoryService.js
@@ -1,7 +1,6 @@
 const url = "http://localhost:4300";
 
 const normalisePhoneResponse = ({ phones } = { phones: [] }) => phones;
-const normalizeLaptopResponse = ({ laptops } = { laptops: [] }) => laptops;
 
 export const findPhoneOffers = () => {
   return fetch(`${url}/categories/phones`, {
@@ -16,6 +15,8 @@ export const findPhoneOffers = () => {
     .then(normalisePhoneResponse);
 };
 
+const normalizeLaptopResponse = ({ laptops } = { laptops: [] }) => laptops;
+
 export const findLaptopOffers = () => {
   return fetch(`${url}/categories/laptops`, {
     method: "GET",
@@ -27,4 +28,19 @@ export const findLaptopOffers = () => {
       return response.json();
     })
     .then(normalizeLaptopResponse);
+};
+
+const normalizeTvResponse = ({ tvs } = { tvs: [] }) => tvs;
+
+export const findTvOffers = () => {
+  return fetch(`${url}/categories/tvs`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then(normalizeTvResponse);
 };
